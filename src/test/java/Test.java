8b53b44896512b8197f66c3927fae3b9a9c191ab
aj <yakamoz_lady@mail.ru>
2020-03-24 17:48:33 -0500

    This is from Ajara

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("This is for first commit");
+        System.out.println("Ajara's code commit");
 
     }
 
