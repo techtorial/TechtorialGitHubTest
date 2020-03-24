4a1ea79bdf3fea38cc11c43c6357b5efa57359fa
techtorial <muammerturan@techtorialacademy.com>
2020-03-24 17:56:47 -0500

    Muammer's code new changes

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
         System.out.println("This is for first commit");
         System.out.println("Ajara's code commit");
+        System.out.println("This is for Yusuf's update ");
+        System.out.println("Muammer's code changes");
 
     }
 }
