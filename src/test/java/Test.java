064d612f31bdf35349713fb307c91dd22a8b367b
aj <yakamoz_lady@mail.ru>
2020-03-28 16:36:48 -0500

    Ajara commiting

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -9,8 +9,12 @@ public class Test {
         System.out.println("New changes from Ajara");
         System.out.println("Muammer's second changes");
         System.out.println("New anges");
+        System.out.println("My stashed info");
+
+
         System.out.println("Muammer's third changes");
         System.out.println("Muammer's fourth changes");
+        System.out.println("working in coco branch");
 
     }
 }
